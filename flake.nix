5f5858219446483cfb455f4c035687c85e557d65
Elmar Schug <elmar.schug@jayware.org>
2025-07-04 10:37:16 +0200

    Added systemd service to flake output.

--- flake.nix
+++ flake.nix
@@ -23,8 +23,94 @@
         craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
           p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
         );
+        src = craneLib.cleanCargoSource ./.;
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          CARGO_PROFILE = "release";
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        rusty-nix = craneLib.buildPackage (
+          commonArgs // {
+            inherit cargoArtifacts;
+          }
+        );
       in
       {
+        packages = {
+          inherit rusty-nix;
+          default = rusty-nix;
+        };
+        nixosModules.rusty-nix = { lib, config, ... }:
+          let
+            cfg = config.rusty-nix;
+          in
+          {
+            options.rusty-nix = {
+              enable = lib.mkEnableOption "Enables rusty-nix service.";
+              port = lib.mkOption {
+                type = lib.types.port;
+                default = 8121;
+              };
+              data-directory = lib.mkOption {
+                type = lib.types.path;
+                default = "/var/lib/rusty-nix/";
+              };
+              user = {
+                name = lib.mkOption {
+                  type = lib.types.str;
+                  default = "rusty";
+                };
+                group = lib.mkOption {
+                  type = lib.types.str;
+                  default = "rusty";
+                };
+              };
+              service = {
+                name = lib.mkOption {
+                  type = lib.types.str;
+                  default = "rusty-nix";
+                };
+              };
+            };
+            config = lib.mkIf cfg.enable {
+              users.users."${cfg.user.name}" = {
+                group = "${cfg.user.group}";
+                isSystemUser = true;
+                linger = true;
+              };
+              users.groups."${cfg.user.group}" = {
+              };
+              systemd.services."${cfg.service.name}" = {
+                enable = true;
+                description = "The rusty-nix service.";
+                restartIfChanged = true;
+                environment = {
+                  RUSTY_NIX_PORT="${toString(cfg.port)}";
+                  RUSTY_NIX_DATA="${cfg.data-directory}";
+                };
+                unitConfig = {
+                  Type = "simple";
+                  # ...
+                };
+                serviceConfig = {
+                  ExecStart = "${rusty-nix}/bin/hello-rusty-nix";
+                  User = "${cfg.user.name}";
+                  Group = "${cfg.user.group}";
+                };
+                wantedBy = [ "multi-user.target" ];
+              };
+              systemd.tmpfiles.settings."${cfg.service.name}-data-directory" = {
+                  "${cfg.data-directory}" = {
+                    d = {
+                      user = "${cfg.user.name}";
+                      group = "${cfg.user.group}";
+                      mode = "0755";
+                    };
+                };
+              };
+            };
+          };
         devShells.default = craneLib.devShell {
           # additional packages for the dev shell
           packages = with pkgs; [
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let environment = env::vars()
+        .filter(|env| env.0.starts_with("RUSTY_NIX_"))
+        .collect::<Vec<_>>();
+
+    for (key, value) in environment {
+        println!("{key}={value}");
+    }
+
+    println!("Hello, Rusty Nix!");
+
+    Ok(())
 }
